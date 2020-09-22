51b707897a7667e520e683368ce0d8cd40b884d4
j3rrr <47906720+j3rrr@users.noreply.github.com>
2020-09-22 18:56:59 +0200

    Updates

---
+++ component---src-pages-404-js-65374dc4a633d1891b2f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Wbzz"),c=n("Bl7J"),r=n("hIod");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"404"}),l.a.createElement("h1",null,"Page not Found"),l.a.createElement("p",null,l.a.createElement(o.a,{to:"/"},"Get Home")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-65374dc4a633d1891b2f.js.map
