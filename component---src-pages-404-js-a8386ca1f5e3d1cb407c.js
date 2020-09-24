f2c7cc10f3499a2b328708e44774752df13225e5
j3rrr <47906720+j3rrr@users.noreply.github.com>
2020-09-24 07:18:23 +0200

    Updates

---
+++ component---src-pages-404-js-a8386ca1f5e3d1cb407c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Wbzz"),c=n("Bl7J"),r=n("hIod");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"404"}),l.a.createElement("h1",null,"Page not Found"),l.a.createElement("p",null,l.a.createElement(o.Link,{to:"/"},"Get Home")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a8386ca1f5e3d1cb407c.js.map
